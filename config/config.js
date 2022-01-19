6f316029d365628f11c0c1d4127401dd586e53a7
fairtoplay <97588978+fairtoplay@users.noreply.github.com>
2022-01-19 17:22:34 +0630

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,20 @@
+const config = {
+    app_name: 'PiggieGhost',
+    app_description: 'A generative collection of 777 PiggieGhost The PiggieGhost are stored as ERC-721 tokens on Polygon and hosted on IPFS.',
+    collection_file_name: 'collection.json',
+    collection_contract_address: '0xeCbd4de8c49c6caA2bcfbc8F8B18Ecbc24F8DADe',
+    collection_name: 'PiggieGhost',
+    collection_description: 'A generative collection of 777 PiggieGhost The PiggieGhost are stored as ERC-721 tokens on Polygon and hosted on IPFS.',
+    collection_id_from: 0,
+    ignore_traits: ['date'], 
+    sqlite_file_name: 'database.sqlite',
+    ga: 'G-BW69Z04YTP',
+    main_og_image: 'https://onedaypunk-rarity-tool.herokuapp.com/images/og.png',
+    item_path_name: 'punk',
+    page_item_num: 60,
+    content_image_is_video: false,
+    content_image_frame: 'circle', // circle, rectangle
+    use_wallet: false
+};
+
+module.exports = config;
